d158d27d34c63de46238010ec88fec33ce66f2dd
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-06-07 20:48:43 -0300

    feat: Add new components for big card, menu bar, menu title, and small card
    The code changes include adding new HTML and TypeScript files for the following components: big card, menu bar, menu title, and small card. These components are essential for the functionality and layout of the application.
    
    Recent user commits:
    - Update image1.png
    - chore: add CLI analytics tracking ID
    - initial commit
    
    Recent repository commits:
    - Update image1.png
    - chore: add CLI analytics tracking ID
    - initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent]
 })
 export class AppComponent {
   title = 'blog-with-angular';
