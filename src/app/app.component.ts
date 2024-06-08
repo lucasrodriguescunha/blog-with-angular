c100023c2ca51383e93d4cd687a3c1f36fccc521
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-06-08 18:12:51 -0300

    chore: Add new components for big card, menu bar, menu title, and small card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from "./components/small-card/small-card.component";
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent]
+    imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'blog-with-angular';
