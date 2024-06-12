2ecdffcb43eb914cedfddc45286452173ac49f60
Lucas Rodrigues Cunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2024-06-12 17:30:48 -0300

    chore: Update big card component to use dynamic data
    The code changes in the big card component include updating the HTML template to use dynamic data for the photo cover, card title, and card description. This allows for more flexibility in displaying different content in the big card component.
    
    Recent user commits:
    - chore: Update CSS styles for menu title component
    - chore: Update CSS styles for home, big card, menu title, and small card components
    
    Recent repository commits:
    - chore: Update CSS styles for menu title component
    - chore: Update CSS styles for home, big card, menu title, and small card components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'big-card',
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css',
 })
-export class BigCardComponent {}
+export class BigCardComponent implements OnInit {
+  @Input()
+  photoCover: string = '';
+
+  @Input()
+  cardTitle: string = '';
+
+  @Input()
+  cardDescription: string = '';
+
+  ngOnInit(): void {
+    throw new Error('Método não implementado.');
+  }
+}
